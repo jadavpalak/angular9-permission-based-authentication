9a68d4d355808c071862fdb2a142d9d7ab6b3021
Palak Jadav <palakjadav.1996@gmail.com>
2020-06-15 17:57:28 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   TOKEN_COOKIE_NAME: 'authToken',
   USER_COOKIE_NAME: 'currentUser',
-  COOKIE_DOMAIN: 'localhost',
+  COOKIE_DOMAIN: 'kind-nightingale-38a877.netlify.app',
 };
